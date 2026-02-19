9bce2cc38404a8279d6ee289cc7abc4357704f11
Esteban DEROUSSIN <esteban.deroussin@icloud.com>
2026-02-19 22:49:33 +0100

    feat: création des différentes routes de l'app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Location from "./pages/Location";
+
 function App() {
-    return <>hello</>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/location/:id" element={<Location />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
